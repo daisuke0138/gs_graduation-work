5d07809792899402e1d92ae7660ca6ad1cb5d1de
04_W_daisuke_aramaki <daisuke_arsp@hotmail.com>
2024-09-18 20:30:04 +0900

    push

--- front-work/src/lib/apiClient.ts
+++ front-work/src/lib/apiClient.ts
@@ -5,7 +5,7 @@ const apiClient = axios.create({
     // baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
     baseURL: process.env.NODE_ENV === 'development'
         ? 'http://localhost:3001/api'
-        : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
+        : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/`,
     headers: {
         "Content-Type": "application/json",
     },
