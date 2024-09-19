a1c1c3627af7dd335c0730e072887ddbf1338bd0
04_W_daisuke_aramaki <daisuke_arsp@hotmail.com>
2024-09-19 20:37:05 +0900

    push

--- front-work/src/components/Login/index.tsx
+++ front-work/src/components/Login/index.tsx
@@ -20,14 +20,15 @@ const Login = () => {
 
         console.log(email);
         console.log(password);
-
+        console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
+        console.log(process.env.NEXT_PUBLIC_API_BASEURL);
         // 追記 APIにデータを送りましょう！
         try {
+            console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
             const response = await apiClient.post("/api/auth/login", {
                 email,
                 password,
             });
-
             const token = response.data.token;
             console.log(token);
             // ここで使用する
