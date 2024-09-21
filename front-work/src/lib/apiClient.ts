fd847a45a6a14a01f7cfb65821322fadfba42764
04_W_daisuke_aramaki <daisuke_arsp@hotmail.com>
2024-09-21 16:41:56 +0900

    push

--- front-work/src/lib/apiClient.ts
+++ front-work/src/lib/apiClient.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:3001/backend-api",
+    // baseURL: "http://localhost:3001/backend-api",
     // // baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
-    // baseURL: '/backend-api',
+    baseURL: '/backend-api',
     // baseURL: process.env.NEXT_PUBLIC_API_BASEURL || `https:${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
     // baseURL: process.env.NODE_ENV === 'development'
     //     ? 'http://localhost:3001/api'
